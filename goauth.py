0b3fe6e25be84c1cabaa869b946d0e65a0fff711
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-02-14 12:47:45 +0100

    moved more code

--- goauth.py
+++ goauth.py
@@ -38,3 +38,28 @@ class OAuthReturnHandler:
 		while self.httpd_access_token_callback == None:
 			self.httpd.handle_request()
 		return self.httpd_access_token_callback
+
+
+def authorize(scopes, client):
+	CONSUMER_KEY = 'anonymous'
+	CONSUMER_SECRET = 'anonymous'
+
+	oauthreturnhandler = OAuthReturnHandler()
+	request_token = client.GetOAuthToken(
+		scopes, oauthreturnhandler.oauth_callback_url, CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+
+	loginurl = request_token.generate_authorization_url()
+	loginurl = str(loginurl)
+	print "* open oauth login page"
+	import webbrowser; webbrowser.open(loginurl)
+
+	print "* waiting for redirect callback ...",
+	httpd_access_token_callback = oauthreturnhandler.wait_callback_response()
+	print "done"
+
+	import gdata.gauth
+	request_token = gdata.gauth.AuthorizeRequestToken(request_token, httpd_access_token_callback)
+
+	# Upgrade the token and save in the user's datastore
+	access_token = client.GetAccessToken(request_token)
+	client.auth_token = access_token
--- oauth-test.py
+++ oauth-test.py
@@ -1,40 +1,13 @@
 #!/usr/bin/python
 
-import gdata.gauth
 import gdata.contacts.client
-
-CONSUMER_KEY = 'anonymous'
-CONSUMER_SECRET = 'anonymous'
-SCOPES = [ "http://www.google.com/m8/feeds/" ] # contacts
 
 client = gdata.contacts.client.ContactsClient(source='Test app')
 
 import goauth
-oauthreturnhandler = goauth.OAuthReturnHandler()
-request_token = client.GetOAuthToken(
-    SCOPES, oauthreturnhandler.oauth_callback_url, CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
 
-# When using HMAC-SHA1, you need to persist the request_token in some way.
-# You'll need the token secret when upgrading to an access token later on.
-# In Google App Engine, you can use the AeSave helper:
-# gdata.gauth.AeSave(request_token, 'myKey')
-
-loginurl = request_token.generate_authorization_url()
-loginurl = str(loginurl)
-print "* open oauth login page"
-import webbrowser; webbrowser.open(loginurl)
-
-print "* waiting for redirect callback ...",
-httpd_access_token_callback = oauthreturnhandler.wait_callback_response()
-print "done"
-
-
-request_token = gdata.gauth.AuthorizeRequestToken(request_token, httpd_access_token_callback)
-
-# Upgrade the token and save in the user's datastore
-access_token = client.GetAccessToken(request_token)
-client.auth_token = access_token
-
+SCOPES = [ "http://www.google.com/m8/feeds/" ] # contacts
+goauth.authorize(SCOPES, client)
 
 def PrintFeed(feed):
   for i, entry in enumerate(feed.entry):
